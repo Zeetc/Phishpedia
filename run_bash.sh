e9b46d4674d6553ba9ed3fbdf305e43cc835799d
Huang Qiyue <qiyue2001@gmail.com>
2023-12-19 20:08:47 +0800

    update

--- run_bash.sh
+++ run_bash.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+FILEDIR=$(pwd)
+CONDA_BASE=$(conda info --base)
+source "$CONDA_BASE/etc/profile.d/conda.sh"
 
 if ! conda info --envs | grep -q "Phishpedia"; then
   echo "The Phishpedia Conda environment does not exist."
   exit 1
 fi
 
-source activate Phishpedia
+conda activate Phishpedia
 python ./phishpedia/test.py
