c015b2710f598c3e3451b11396ffd38efac602a9
Zeetc <Zeetc@github.com>
2023-12-21 07:16:26 +0000

    更改下载文件逻辑

--- setup.sh
+++ setup.sh
@@ -56,20 +56,42 @@ fi
 pip install -v .
 # package_location=$(pip show phishpedia | grep Location | awk '{print $2}')
 
+# 检查每个字符串是否包含error关键词
+check_string_contains_error() {
+    # 定义关键词
+    keyword="error"
+    if [[ $1 == *"$keyword"* ]]; then
+        echo "A total of 4 files are required. File $2 download failed, program exited."
+        exit 1
+    fi
+}
+
 if [ -z "Phishpedia" ]; then
   echo "Package Phishpedia not found in the Conda environment myenv."
   exit 1
 else
-  echo "Going to the directory of package Phishpedia in Conda environment myenv."
-  mkdir -p "phishpedia/src/detectron2_pedia/output/rcnn_2"
-  cd "phishpedia/src/detectron2_pedia/output/rcnn_2" || exit 1
-  pip install gdown
-  gdown --id 1tE2Mu5WC8uqCxei3XqAd7AWaP5JTmVWH
-  mkdir -p "phishpedia/src/siamese_pedia/"
-  cd "phishpedia/src/siamese_pedia/" || exit 1
-  gdown --id 1H0Q_DbdKPLFcZee8I14K62qV7TTy7xvS
-  gdown --id 1fr5ZxBKyDiNZ_1B6rRAfZbAHBBoUjZ7I
-  gdown --id 1qSdkSSoCYUkZMKs44Rup_1DPBxHnEKl1
+  # 判断是否存在第一个参数，此处仅判断有无缓存，存在参数则表示有缓存
+  if [ -z "$1" ]; then
+    echo "Cache loaded, no need to download files from google drive"
+  else
+    echo "Going to the directory of package Phishpedia in Conda environment myenv."
+    mkdir -p "phishpedia/src/detectron2_pedia/output/rcnn_2"
+    cd "phishpedia/src/detectron2_pedia/output/rcnn_2" || exit 1
+    pip install gdown
+    download_file_1=$(gdown --id 1tE2Mu5WC8uqCxei3XqAd7AWaP5JTmVWH)
+    check_string_contains_error "$download_file_1" 1
+    mkdir -p "phishpedia/src/siamese_pedia/"
+    cd "phishpedia/src/siamese_pedia/" || exit 1
+
+    download_file_2=$(gdown --id 1H0Q_DbdKPLFcZee8I14K62qV7TTy7xvS)
+    check_string_contains_error "$download_file_2" 2
+
+    download_file_3=$(gdown --id 1fr5ZxBKyDiNZ_1B6rRAfZbAHBBoUjZ7I)
+    check_string_contains_error "$download_file_3" 3
+
+    download_file_4=$(gdown --id 1qSdkSSoCYUkZMKs44Rup_1DPBxHnEKl1)
+    check_string_contains_error "$download_file_4" 4
+  fi
 fi
 
 # Replace the placeholder in the YAML template
